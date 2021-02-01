f0fe6814263fc56394da9cf2a5d93f9d2571e7a2
omer shalgi <oshalgi101@gmail.com>
2021-02-01 14:45:05 +0200

    this version

---
+++ TamaguchiServer/DataTransferObjects/PlayerDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TamaguchiServer.DataTransferObjects
+{
+    public class PlayerDTO
+    {
+        public PlayerDTO()
+        {
+
+        }
+        public string Email { get; set; }
+    }
+}
